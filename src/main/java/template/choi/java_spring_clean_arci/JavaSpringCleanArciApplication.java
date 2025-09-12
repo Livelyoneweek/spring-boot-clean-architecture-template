53124a7615f812145e25881383ea09a63937cb6b
Livelyoneweek <wlvvwhs@naver.com>
2025-09-12 18:13:01 +0900

    ✅ Project init
    Add dependency, project

---
+++ src/main/java/template/choi/java_spring_clean_arci/JavaSpringCleanArciApplication.java
@@ -0,0 +1,13 @@
+package template.choi.java_spring_clean_arci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringCleanArciApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringCleanArciApplication.class, args);
+    }
+
+}
---
+++ src/test/java/template/choi/java_spring_clean_arci/JavaSpringCleanArciApplicationTests.java
@@ -0,0 +1,13 @@
+package template.choi.java_spring_clean_arci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringCleanArciApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
